595a20b172957957397b398bc699131f2ac6e1bb
donihadimas <donihadimasaprilian@gmail.com>
2024-03-07 11:14:25 +0700

    feat: add feature pagination and search

--- app/services/mongoose/categories.js
+++ app/services/mongoose/categories.js
@@ -1,10 +1,38 @@
 const Categories = require('../../api/v1/master-data/categories/model')
 const { BadRequestError, NotFoundError } = require(".././../errors")
 
-const getAllCategories = async () => {
-    const result = await Categories.find()
+const getAllCategories = async (req) => {
+    const { page, pageSize, search } = req.query;
 
-    return result;
+    if (!page || !pageSize) {
+        let query = {};
+        if (search) {
+            query = { categoryName: { $regex: new RegExp(search, "i") } };
+        }
+        const allCategories = await Categories.find(query);
+        return {
+            totalData: allCategories.length,
+            categories: allCategories,
+        };
+    }
+
+    let query = {};
+    if (search) {
+        query = { categoryName: { $regex: new RegExp(search, "i") } };
+    }
+    
+    const totalCount = await Categories.countDocuments(query);
+
+    const result = await Categories.find(query)
+        .skip((page - 1) * pageSize)
+        .limit(pageSize);
+
+    return {
+        totalData: totalCount,
+        page,
+        pageSize,
+        categories: result,
+    };
 }
 
 const getOneCategories = async (req) => {
@@ -18,13 +46,13 @@ const getOneCategories = async (req) => {
 }
 
 const createCategories = async (req) => {
-    const { categoryName } = req.body;
+    const { categoryName, totalProductInCategory } = req.body;
 
     const checkCategory = await Categories.findOne({ categoryName });
 
     if (checkCategory) throw new BadRequestError("Kategori sudah ada");
 
-    const result = await Categories.create({ categoryName })
+    const result = await Categories.create({ categoryName, totalProductInCategory })
     return result
 }
 
