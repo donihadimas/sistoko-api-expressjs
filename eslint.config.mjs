c5d5a1ff729bf64abdbf950f991f919099b5935d
donihadimas <donihadimasaprilian@gmail.com>
2024-05-11 08:47:49 +0700

    feat: configure eslint and pretier

--- eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-const globals = require("globals");
-const pluginJs = require("@eslint/js");
-
-
-export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-];
